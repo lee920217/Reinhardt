cb34482d36480f8a0177a4e8b91e2d2ab2d1cdf3
lee920217 <francislee920217@gmail.com>
2020-03-08 20:30:52 +0000

    update policy

--- reinhardt/src/components/common/CookieMask.vue
+++ reinhardt/src/components/common/CookieMask.vue
@@ -1,8 +1,27 @@
 <template>
   <div class="cookie-mask-container">
     <div class="content">
-      We use cookies to give you the best online experience.
-      Please let us know if you agree to all of these cookies.
+      <div class="content-header">
+        Cookie
+      </div>
+      We use cookies to give you the best online experience. Please let us know if you agree to all
+      of these cookies.
+    </div>
+    <div class="notice-main-ct">
+      <div class="notice-header">
+        Notice
+      </div>
+      <div class="notice-body">
+        <div class="notice-item">· All these data and news may <b>NOT</b> accurate and updated</div>
+        <div class="notice-item">
+          · The developers will work hard on data updated, but they are <b>NOT</b> responsible for
+          any data accuracy
+        </div>
+        <div class="notice-item">
+          · These data on the website can <b>NOT</b> be reference, please use the data on GOV.UK
+          directly
+        </div>
+      </div>
     </div>
     <div class="button" v-on:touchstart="hideBtn">Yes, I agree with that</div>
   </div>
@@ -12,12 +31,12 @@
 export default {
   name: "CookieMask",
   methods: {
-    hideBtn () {
+    hideBtn() {
       const self = this;
-      self.$emit('hideMask', '')
+      self.$emit("hideMask", "");
     }
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
@@ -25,18 +44,41 @@ $designWidth: 750;
 @import "@/assets/library/px2rem.scss";
 .cookie-mask-container {
   position: fixed;
-  top: px2rem(900);
+  top: px2rem(700);
   left: 0;
   right: 0;
   bottom: 0;
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  justify-content: space-around;
   background-color: #ffffff;
+  .content-header {
+    font-size: px2rem(50);
+    text-align: left;
+    font-weight: 500;
+  }
+
   z-index: 30;
   .content {
-    padding: 0 px2rem(24) px2rem(24) px2rem(24);
+    text-align: left;
+    padding: 0 px2rem(32) px2rem(32) px2rem(32);
     font-size: px2rem(30);
+  }
+  .notice-main-ct {
+    padding: 0 px2rem(32);
+    .notice-header {
+      text-align: left;
+      font-weight: 500;
+    }
+    .notice-body {
+      padding-bottom: px2rem(32);
+      text-align: left;
+      font-weight: 300;
+      font-size: px2rem(30);
+      .notice-item {
+        margin-bottom: px2rem(20);
+      }
+    }
   }
   .button {
     width: px2rem(696);
