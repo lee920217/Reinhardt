0c3c08904f226425fd640043b45f240553ffb71d
lee920217 <francislee920217@gmail.com>
2020-04-04 20:17:37 +0800

    update animation

--- reinhardt/src/components/Animation.vue
+++ reinhardt/src/components/Animation.vue
@@ -9,6 +9,12 @@
         <span>人</span>
       </div>
     </div>
+    <div class="progree-bar">
+      <div class="bar">
+        <div class="data-tri" :style="`left: ${arrowLeft}%;transition: 1s`"></div>
+        <div class="date" :style="`left: ${arrowLeft-3}%;transition: 1s`">{{date}}</div>
+      </div>
+    </div>
     <div class="map-ct">
       <highcharts :constructor-type="'mapChart'" :options="UKMapSettings" ref="ukmap"></highcharts>
     </div>
@@ -27,7 +33,8 @@ export default {
       UKMapSettings: UKMapSettings,
       date: "03-05",
       count: 0,
-      animationCounter: null
+      animationCounter: null,
+      arrowLeft: 0
     }
   },
   mounted () {
@@ -55,13 +62,15 @@ export default {
       let length = historyData.length;
       self.animationCounter = setInterval(() => {
         if (index >= length) {
-          clearInterval(animationCounter);
+          clearInterval(self.animationCounter);
+          self.arrowLeft = 98;
           return
         }
         let currentDate = historyDate[index];
         self.date = currentDate;
         let dateIndex = dateList.indexOf(currentDate);
         let currentCount = totalNum[dateIndex];
+        self.arrowLeft = (index / length) * 100;
         // self.count = currentCount;
         // console.log('c', self.count);
         // console.log('n', currentCount);
@@ -117,9 +126,40 @@ $designWidth: 750;
     top: px2rem(24);
     right: px2rem(24);
   }
+  .progree-bar {
+    position: relative;
+    width: 100%;
+    height: px2rem(2);
+    margin: 0 auto px2rem(100) auto;
+    .bar {
+      position: relative;
+      width: px2rem(650);
+      height: px2rem(2);
+      background-color: #fc4b4b;
+      margin: 0 auto;
+    }
+    .data-tri {
+      position: absolute;
+      left: 0;
+      width: 0;
+      height: 0;
+      border: px2rem(10) solid transparent;
+      border-bottom: px2rem(10) solid #f2f2f2;
+      font-size: px2rem(24);
+    }
+    .date {
+      position: absolute;
+      width: px2rem(80);
+      left: 0;
+      top: px2rem(20);
+      font-size: px2rem(24);
+      color: #f2f2f2;
+      text-align: left;
+    }
+  }
   .data-and-num {
-    margin-top: px2rem(200);
-    margin-bottom: px2rem(200);
+    margin-top: px2rem(100);
+    margin-bottom: px2rem(100);
     color: #f2f2f2;
     font-weight: 900;
     .count {
--- reinhardt/src/views/Index.vue
+++ reinhardt/src/views/Index.vue
@@ -205,7 +205,7 @@ export default {
   },
   data () {
     return {
-      animationStatus: false,
+      animationStatus: true,
       UKMapSettings: UKMapSettings,
       ChartSettings: ChartSettings,
       IncreaseChartSettings: IncreaseChartSettings,
